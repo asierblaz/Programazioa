8d96832c7ff9406026ea54ebe06d7bb3b7b94ee3
Asier <blazgar@gmail.com>
2021-01-17 14:09:32 +0100

    Update ProgramaNagusia.java

--- UD03/2020-02-18Hasierakoa/src/exekutagarriak/ProgramaNagusia.java
+++ UD03/2020-02-18Hasierakoa/src/exekutagarriak/ProgramaNagusia.java
@@ -17,36 +17,63 @@ public class ProgramaNagusia {
     public static void initialize() {
 
         Author rowling = new Author("J.K.Rowling", 'f');
+        idazleak[0] = rowling;
         liburutegia.add(new Book("Harry Potter", rowling, 99.99, 2850));
 
         Author cris = new Author("Agatha Christie", 'f');
-        
+        idazleak[1] = cris;
         liburutegia.add(new Book("Asesinato en el Oriente Express", cris, 9.95, 275));
         liburutegia.add(new Book("Muerte en el Nilo", cris, 9.95, 300));
         liburutegia.add(new Book("10 negritos", cris, 9.95, 325));
 
         Author ibanez = new Author("Francisco Ibañez", 'm');
+        idazleak[2] = ibanez;
         liburutegia.add(new Komikia(true, "Mortadelo y Filemon", ibanez, 19.95, 75));
 
         Author atwood = new Author("Margaret Atwood", 'f');
+        idazleak[3] = atwood;
+
         liburutegia.add(new Book("El cuento de la criada", atwood, 24.95, 410));
         liburutegia.add(new Book("Cat's eye", atwood, 6.95, 78));
 
         Author satrapi = new Author("Marjane Satrapi", 'f');
+        idazleak[4] = satrapi;
+
         liburutegia.add(new Komikia(true, " Persepolis", satrapi, 19.95, 75));
 
         Author landa = new Author("Mariasun Landa", 'f');
+        idazleak[5] = landa;
+
         liburutegia.add(new Book("Azken Balada", landa, 10.95, 225));
 
         Author lee = new Author("Harper Lee", 'f');
+        idazleak[6] = lee;
+
         liburutegia.add(new Book("Matar a un ruiseñor", lee, 15.95, 450));
 
         Author mary = new Author("Mary Shelley", 'f');
+        idazleak[7] = mary;
+
         liburutegia.add(new Book("Frankesnstein", mary, 19.95, 300));
 
     }
 
     public static void jaiotzeDataBete() {
+
+        System.out.print("Zein idazleren jaiotze data nahi duzu bete (Idatzi posizioa)? ");
+        Scanner in = new Scanner(System.in);
+        int pos = in.nextInt() - 1;
+        System.out.print("Sartu " + idazleak[pos].getName() + " idazlearen jaiotze data (uuuu-hh-ee): ");
+        try {
+            idazleak[pos].setBirthday(in.next());
+            System.out.println("Jaiotze-data ondo erregistratu da.");
+        } catch (Exception e) {
+            System.out.println("Data okerra.");
+            System.out.println("Hurrengo baten sartuko duzu data.");
+
+        }
+        
+        System.out.println("");
 
     }
 
