300e31e87ce8f0331f84260c8f4e921c499180b7
Asier <blazgar@gmail.com>
2021-03-11 10:18:18 +0100

    hiztegiasql

---
+++ UD04/HiztegiaSQLite/db/HiztegiaDBSortzekoAginduak.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Terminoak(
+id int PRIMARY KEY,
+euskaraz VARCHAR(20),
+gazteleraz VARCHAR(20))
+
+
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('sagarra', 'manzana');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('madaria', 'pera');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('marrubia', 'fresa');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('baloia', 'balon');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('xagua', 'raton');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('ordenagailua', 'ordenador');
+INSERT INTO "Terminoak" ("euskaraz", "gazteleraz") VALUES ('kalkulagailua', 'calculadora');
---
+++ UD04/HiztegiaSQLite/src/model/SQLiteKudeatu.java
@@ -0,0 +1,91 @@
+package model;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author blazquez.asier
+ */
+public class SQLiteKudeatu {
+
+    private static String url = "jdbc:sqlite:db/Hiztegia.db";;
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+             
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+
+ 
+    private static Connection connect2() {
+        // SQLite connection string
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    public static void terminoakImprimatu(){
+        String sql = "SELECT id, euskaraz, gazteleraz FROM Terminoak";
+        
+        try ( Connection conn = connect2();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("euskaraz") + "\t" +
+                                   rs.getString("gazteleraz"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void terminoaGehitu(String euskaraz, String gazteleraz) {
+        String sql = "INSERT INTO Terminoak(euskaraz,gazteleraz) VALUES(?,?)";
+
+        try (Connection conn = connect2();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, euskaraz);
+            pstmt.setString(2, gazteleraz);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+     
+}
