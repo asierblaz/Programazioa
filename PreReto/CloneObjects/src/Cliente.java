2cb660a85d5a194b2475413de471fc39381c880d
Asier <blazgar@gmail.com>
2021-04-27 12:22:25 +0200

    colections

---
+++ PreReto/CloneObjects/src/Cliente.java
@@ -0,0 +1,77 @@
+
+import java.util.Objects;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author blazquez.asier
+ */
+public class Cliente {
+    private String nombre;
+    private String n_cuenta;
+    private double saldo;
+
+    public Cliente(String nombre, String n_cuenta, double saldo) {
+        this.nombre = nombre;
+        this.n_cuenta = n_cuenta;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getN_cuenta() {
+        return n_cuenta;
+    }
+
+    public void setN_cuenta(String n_cuenta) {
+        this.n_cuenta = n_cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.n_cuenta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.n_cuenta, other.n_cuenta)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    
+    
+}
